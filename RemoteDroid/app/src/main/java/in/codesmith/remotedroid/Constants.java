31294ed2dbedb7b31c84105a683845ab1f406188
Hector <libaobao897@gmail.com>
2018-11-22 16:52:30 +0100

    Android VLC

---
+++ RemoteDroid/app/src/main/java/in/codesmith/remotedroid/Constants.java
@@ -0,0 +1,13 @@
+package in.codesmith.remotedroid;
+
+/**
+ * Created by Sourabh Soni on 07-03-2015.
+ */
+public class Constants {
+    public static final String SERVER_IP = "10.151.192.89";
+    public static final int SERVER_PORT = 8888;
+    public static final String PLAY="play";
+    public static final String NEXT="next";
+    public static final String PREVIOUS="previous";
+    public static final String MOUSE_LEFT_CLICK="left_click";
+}
