710746c47bf28baf5b60ee189bd536a17f375f20
Hector <libaobao897@gmail.com>
2019-01-16 02:40:15 +0100

    Nuevas mejoras
    Nuevo Toast, se han añadido cosas en los créditos, se ha cambiado el nombre de la aplicación y ajustado las orientaciones en varios layout (al poner en horizontal el 3 en raya y el dado, se descolocaban), por lo que ahora solo se ponen en vertical aunque gires el móvil

--- Nuevo/Multi/app/src/main/java/com/example/hguti/multi/MainActivity.java
+++ Nuevo/Multi/app/src/main/java/com/example/hguti/multi/MainActivity.java
@@ -3,15 +3,33 @@ package com.example.hguti.multi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 9;
+        final Button button = findViewById(R.id.Njoy);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                counter ++;
+                if (counter ==10){
+                Toast toast = Toast.makeText(getApplicationContext(), "Relájate y disfruta de la aplicación!", Toast.LENGTH_SHORT);
+                toast.show();
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                counter = 0;
+            }
+            }
+        });
     }
+
+
     public void GotoPiano(View view){
         Intent solicitud = new Intent(this, Piano.class);
         startActivity(solicitud);
